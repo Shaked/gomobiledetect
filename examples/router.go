23b2af1ceb2adc477d3cd2e4082c99cbaa70a105
Shaked Klain <pigiman@gmail.com>
2014-02-09 02:56:39 +0100

    added new example using package together with an implementation of ServeHTTP (fixed issue #6)

---
+++ examples/router.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+
+	"github.com/Shaked/gomobiledetect"
+)
+
+//route manager
+type route struct {
+	re      *regexp.Regexp
+	handler func(http.ResponseWriter, *http.Request, []string, *gomobiledetect.MobileDetect)
+}
+
+type RouterHandler struct {
+	routes []*route
+	detect *gomobiledetect.MobileDetect
+}
+
+func (h *RouterHandler) AddRoute(re string, handler func(http.ResponseWriter, *http.Request, []string, *gomobiledetect.MobileDetect)) {
+	r := &route{regexp.MustCompile(re), handler}
+	h.routes = append(h.routes, r)
+}
+
+func (h *RouterHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.detect = gomobiledetect.NewMobileDetect(r, nil)
+	for _, route := range h.routes {
+		matches := route.re.FindStringSubmatch(r.URL.String())
+		if matches != nil {
+			route.handler(rw, r, matches, h.detect)
+			break
+		}
+	}
+}
+
+func homepageHandler(w http.ResponseWriter, r *http.Request, matches []string, detect *gomobiledetect.MobileDetect) {
+	fmt.Fprint(w, "Hello World\n")
+	fmt.Fprintf(w, "Matches %+v\n", matches)
+	fmt.Fprintf(w, "Is Mobile? %+v\n", detect.IsMobile())
+	fmt.Fprintf(w, "Is Tablet? %+v\n", detect.IsTablet())
+}
+
+func main() {
+	reHandler := new(RouterHandler)
+	reHandler.AddRoute("/device/[mobile|desktop]", homepageHandler)
+	http.ListenAndServe(":9999", reHandler)
+}
